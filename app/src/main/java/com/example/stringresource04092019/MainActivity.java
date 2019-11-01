ed6d5a5dd7f188ebfe5a032c38f6e927c03d2b83
phatdroid94 <phatdroid94@gmail.com>
2019-11-01 21:27:48 +0700

    finish

--- app/src/main/java/com/example/stringresource04092019/MainActivity.java
+++ app/src/main/java/com/example/stringresource04092019/MainActivity.java
@@ -3,14 +3,38 @@ package com.example.stringresource04092019;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
+    EditText edtUser,edtPassword;
+    Button btnLogin;
+    TextView txtInfo;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         // tao branch : git branch tennhanh
         // chuyen branch : git checkout tennhanh
+        edtPassword = findViewById(R.id.edittextMatkhau);
+        edtUser = findViewById(R.id.edittextTaikhoan);
+        btnLogin = findViewById(R.id.buttonDangnhap);
+        txtInfo = findViewById(R.id.textviewThongtin);
+
+        btnLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                String tk = edtUser.getText().toString();
+                String mk = edtPassword.getText().toString();
+
+                String tkResource = getResources().getString(R.string.label_info_taikhoan);
+                String mkResource = getResources().getString(R.string.label_info_matkhau);
+                txtInfo.setText(tkResource + tk + "\n" + mkResource + mk);
+            }
+        });
     }
 }
